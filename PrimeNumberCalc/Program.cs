910add7d2c7a35ce06d7e9e595b0986ffdda4cb3
DevLap33 <DevLap33@DESKTOP-5IBIGSP>
2023-01-30 13:19:24 +1300

    Add project files.

---
+++ PrimeNumberCalc/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PrimeNumberCalc
+{
+    class NumberCalc
+    {
+        // This block will create a console screen for the user to input numbers to check what ones are Prime Numbers
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number : ");
+
+            number = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i < number; i++)
+            {
+                if (IsPrime(i))
+                {
+                    Console.Write(i + " is a prime number\n");
+                }
+                else
+                {
+                    Console.Write(i + " is not a prime number\n");
+                }
+            }
+        }
+
+        // This block will check for which number that gets placed in the console to see what ones are Prime Numbers
+        static bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            if (number <= 2) return true;
+            if (number % 2 == 0) return false;
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number));
+
+            for (int i = 3; i <= boundary; i += 2)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
